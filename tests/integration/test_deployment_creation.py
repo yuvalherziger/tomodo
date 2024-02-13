cda0b1c743f9a91cd3f96e79a55f73bdee6f6df5
Yuvi Herziger <yuvi.herziger@mongodb.com>
2024-02-13 17:37:33 +0200

    Debug integration tests

--- tests/integration/test_deployment_creation.py
+++ tests/integration/test_deployment_creation.py
@@ -64,77 +64,77 @@ class TestDeploymentCreation:
                 cleaner.delete_deployment(name=name)
             except EmptyDeployment:
                 pass
-
-    @staticmethod
-    @pytest.mark.parametrize("image_tag", ["5.0", "6.0", "7.0", "latest"])
-    @pytest.mark.parametrize("replicas", [3, 5, 7])
-    @pytest.mark.parametrize("with_arbiter", [True])
-    def test_replica_set_provisioning(image_tag: str, replicas: int, with_arbiter: bool):
-        suffix = secrets.token_hex(2)
-        port = random.randint(27000, 57000)
-        name = f"int-tst-{suffix}"
-        try:
-            config = ProvisionerConfig(
-                name=name,
-                port=port,
-                replica_set=True,
-                replicas=replicas,
-                image_tag=image_tag,
-                arbiter=with_arbiter
-            )
-            provisioner = Provisioner(config=config)
-            deployment: Deployment = provisioner.provision(
-                deployment_getter=Reader().get_deployment_by_name
-            )
-            num_docs = 10
-            assert isinstance(deployment, ReplicaSet), "Not a replica set deployment"
-            mongod = deployment.members[0]
-            seed_collection_data(mongod=mongod, config=config, num_docs=num_docs)
-            estimated_count = get_doc_count(mongod=mongod, config=config)
-            assert estimated_count == num_docs, "Unexpected document count"
-        except Exception as e:
-            assert False, f"Provisioning failed. Exception {str(e)}"
-        finally:
-            cleaner = Cleaner()
-            try:
-                cleaner.delete_deployment(name=name)
-            except EmptyDeployment:
-                pass
-
-    @staticmethod
-    @pytest.mark.parametrize("image_tag", ["5.0", "6.0", "7.0", "latest"])
-    @pytest.mark.parametrize("replicas", [1, 3, 5, 7])
-    @pytest.mark.parametrize("shards", [1, 3])
-    @pytest.mark.parametrize("mongos", [1, 2])
-    def test_sharded_cluster_provisioning(image_tag: str, replicas: int, shards: int, mongos: int):
-        suffix = secrets.token_hex(2)
-        name = f"int-tst-{suffix}"
-        port = random.randint(27000, 57000)
-        try:
-            config = ProvisionerConfig(
-                name=name,
-                port=port,
-                sharded=True,
-                shards=shards,
-                replicas=replicas,
-                mongos=mongos,
-                image_tag=image_tag
-            )
-            provisioner = Provisioner(config=config)
-            deployment: Deployment = provisioner.provision(
-                deployment_getter=Reader().get_deployment_by_name
-            )
-            num_docs = 10
-            assert isinstance(deployment, ShardedCluster), "Not a sharded cluster deployment"
-            mongos = deployment.routers[0]
-            seed_collection_data(mongod=mongos, config=config, num_docs=num_docs)
-            estimated_count = get_doc_count(mongod=mongos, config=config)
-            assert estimated_count == num_docs, "Unexpected document count"
-        except Exception as e:
-            assert False, f"Provisioning failed. Exception {str(e)}"
-        finally:
-            cleaner = Cleaner()
-            try:
-                cleaner.delete_deployment(name=name)
-            except EmptyDeployment:
-                pass
+    #
+    # @staticmethod
+    # @pytest.mark.parametrize("image_tag", ["5.0", "6.0", "7.0", "latest"])
+    # @pytest.mark.parametrize("replicas", [3, 5, 7])
+    # @pytest.mark.parametrize("with_arbiter", [True])
+    # def test_replica_set_provisioning(image_tag: str, replicas: int, with_arbiter: bool):
+    #     suffix = secrets.token_hex(2)
+    #     port = random.randint(27000, 57000)
+    #     name = f"int-tst-{suffix}"
+    #     try:
+    #         config = ProvisionerConfig(
+    #             name=name,
+    #             port=port,
+    #             replica_set=True,
+    #             replicas=replicas,
+    #             image_tag=image_tag,
+    #             arbiter=with_arbiter
+    #         )
+    #         provisioner = Provisioner(config=config)
+    #         deployment: Deployment = provisioner.provision(
+    #             deployment_getter=Reader().get_deployment_by_name
+    #         )
+    #         num_docs = 10
+    #         assert isinstance(deployment, ReplicaSet), "Not a replica set deployment"
+    #         mongod = deployment.members[0]
+    #         seed_collection_data(mongod=mongod, config=config, num_docs=num_docs)
+    #         estimated_count = get_doc_count(mongod=mongod, config=config)
+    #         assert estimated_count == num_docs, "Unexpected document count"
+    #     except Exception as e:
+    #         assert False, f"Provisioning failed. Exception {str(e)}"
+    #     finally:
+    #         cleaner = Cleaner()
+    #         try:
+    #             cleaner.delete_deployment(name=name)
+    #         except EmptyDeployment:
+    #             pass
+    #
+    # @staticmethod
+    # @pytest.mark.parametrize("image_tag", ["5.0", "6.0", "7.0", "latest"])
+    # @pytest.mark.parametrize("replicas", [1, 3, 5, 7])
+    # @pytest.mark.parametrize("shards", [1, 3])
+    # @pytest.mark.parametrize("mongos", [1, 2])
+    # def test_sharded_cluster_provisioning(image_tag: str, replicas: int, shards: int, mongos: int):
+    #     suffix = secrets.token_hex(2)
+    #     name = f"int-tst-{suffix}"
+    #     port = random.randint(27000, 57000)
+    #     try:
+    #         config = ProvisionerConfig(
+    #             name=name,
+    #             port=port,
+    #             sharded=True,
+    #             shards=shards,
+    #             replicas=replicas,
+    #             mongos=mongos,
+    #             image_tag=image_tag
+    #         )
+    #         provisioner = Provisioner(config=config)
+    #         deployment: Deployment = provisioner.provision(
+    #             deployment_getter=Reader().get_deployment_by_name
+    #         )
+    #         num_docs = 10
+    #         assert isinstance(deployment, ShardedCluster), "Not a sharded cluster deployment"
+    #         mongos = deployment.routers[0]
+    #         seed_collection_data(mongod=mongos, config=config, num_docs=num_docs)
+    #         estimated_count = get_doc_count(mongod=mongos, config=config)
+    #         assert estimated_count == num_docs, "Unexpected document count"
+    #     except Exception as e:
+    #         assert False, f"Provisioning failed. Exception {str(e)}"
+    #     finally:
+    #         cleaner = Cleaner()
+    #         try:
+    #             cleaner.delete_deployment(name=name)
+    #         except EmptyDeployment:
+    #             pass
