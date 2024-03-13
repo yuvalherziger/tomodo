5bcf14be64e314c1cca9a6d12020340537d75d85
Yuvi Herziger <yuvi.herziger@mongodb.com>
2024-03-13 20:42:31 +0200

    Faster tag listing

--- tomodo/cli/tags.py
+++ tomodo/cli/tags.py
@@ -1,10 +1,8 @@
 import logging
-from sys import exit
 
 import typer
 
-from tomodo.common.tag_manager import list_tags, group_tags_by_minor_version
-from tomodo.common.util import split_into_chunks
+from tomodo.common.tag_manager import list_tags
 
 cli = typer.Typer(no_args_is_help=True, help="Find MongoDB image tags")
 logger = logging.getLogger("rich")
@@ -15,37 +13,19 @@ logger = logging.getLogger("rich")
     name="list"
 )
 def list_(
-        image_repo: str = typer.Option(
-            default="mongo",
-            help="The MongoDB image name/repo (NOTE: you probably don't want to change it)"
-        ),
-        must_include: str = typer.Option(
+        version: str = typer.Option(
             default=None,
-            help="Filter results. Example: 'tomodo tags list --must-include 7.0'"
-        ),
-        must_exclude: str = typer.Option(
-            default=None,
-            help="Filter results. Example: 'tomodo tags list --must-exclude windows'"
-        ),
-        group: bool = typer.Option(
-            default=True,
-            help="Group results by minor versions"
-        ),
+            help="Filter by version. Must be a valid semantic version (whole or partial). "
+                 "Example: 'tomodo tags list --version 7.0.6'"
+        )
 ):
-    tags = list_tags(repo=image_repo, must_include=must_include, must_exclude=must_exclude)
-    if group:
-        grouped_tags = group_tags_by_minor_version(tags)
-        for minor_ver in grouped_tags.keys():
-            print(f"{minor_ver}:")
-            for tag in grouped_tags[minor_ver]:
-                print(f"  - {tag}")
-    else:
-        tags.sort(reverse=True)
-        results_per_page = 30
-        chunked_tags = split_into_chunks(tags, results_per_page)
-        for chunk in chunked_tags:
-            print("\n".join(chunk))
-            if len(chunk) == results_per_page and typer.confirm(f"Show more?"):
-                continue
-            else:
-                exit(0)
+    results_per_page = 40
+    has_more = True
+    page = 1
+    while has_more:
+        tags, has_more = list_tags(page=page, page_size=results_per_page, version=version)
+        print("\n".join(tags))
+        if has_more and typer.confirm(f"Show more?", default=True):
+            page += 1
+        else:
+            has_more = False
--- tomodo/common/tag_manager.py
+++ tomodo/common/tag_manager.py
@@ -1,12 +1,34 @@
+import datetime
+import os
 import re
 from collections import defaultdict
-from typing import List, Optional, Dict
+from typing import List, Optional, Dict, Tuple
 
 import requests
+from pymongo import MongoClient, UpdateOne
+
+from tomodo.common.util import parse_semver
 
 
-def list_tags(repo: str = "mongo", page: int = 1, page_size: int = 100, must_include: Optional[str] = None,
-              must_exclude: Optional[str] = None) -> List[str]:
+def list_tags(page: int = 1, page_size: int = 40, version: Optional[str] = None) -> Tuple[List[str], bool]:
+    url = "https://eu-central-1.aws.data.mongodb-api.com/app/stuff-yqgey/endpoint/imagetags"
+    params = {}
+    if page:
+        params["page"] = str(page)
+    if page_size:
+        params["pageSize"] = str(page_size)
+    if version is not None and len(version.strip()) > 0:
+        params["version"] = version
+    response = requests.get(url, params=params)
+    if response.status_code != 200:
+        raise Exception("Could not list tags from Docker Hub")
+    tags = response.json()
+    return [t.get("tag") for t in tags[:-1]], len(tags) >= page_size + 1
+
+
+def get_tags_from_dockerhub_api(repo: str = "mongo", page: int = 1, page_size: int = 100,
+                                must_include: Optional[str] = None,
+                                must_exclude: Optional[str] = None) -> List[str]:
     img_tags = []
     url = f"https://hub.docker.com/v2/repositories/library/{repo}/tags"
     params = {"page_size": page_size, "page": page}
@@ -50,3 +72,50 @@ def group_tags_by_minor_version(tags: List[str]) -> Dict[str, List[str]]:
     if len(non_versions):
         version_dict["others"] = non_versions
     return dict(version_dict)
+
+
+def load_tags():
+    mongo_uri = os.environ.get("MONGO_URI")
+    patch_version_pattern = re.compile(r"^(\d+\.\d+.\d+)")
+    minor_version_pattern = re.compile(r"^(\d+\.\d+)")
+    major_version_pattern = re.compile(r"^(\d+)")
+
+    client = MongoClient(mongo_uri)
+    grouped_tags: Dict[str, List[str]] = group_tags_by_minor_version(get_tags_from_dockerhub_api())
+    updates = []
+    db = client.get_database("tomodo")
+    collection = db.get_collection("image_tags")
+    for group in grouped_tags.keys():
+        for tag in grouped_tags[group]:
+            patch_version_match = patch_version_pattern.match(tag)
+            minor_version_match = minor_version_pattern.match(tag)
+            major_version_match = major_version_pattern.match(tag)
+            major, minor, patch = None, None, None
+            if patch_version_match:
+                major, minor, patch = parse_semver(patch_version_match.group(1))
+            elif minor_version_match:
+                major, minor, patch = parse_semver(minor_version_match.group(1))
+            elif major_version_match:
+                major, minor, patch = int(major_version_match.group(1)), None, None
+
+            updates.append(
+                UpdateOne(
+                    {
+                        "tag": tag
+                    },
+                    {
+                        "$set": {
+                            "tag": tag,
+                            "group": group,
+                            "fragments": tag.split("-"),
+                            "release_candidate": "-rc" in tag,
+                            "major": major,
+                            "minor": minor,
+                            "patch": patch,
+                            "imported_at": datetime.datetime.now(tz=datetime.timezone.utc),
+                        }
+                    },
+                    upsert=True
+                )
+            )
+    collection.bulk_write(updates)
